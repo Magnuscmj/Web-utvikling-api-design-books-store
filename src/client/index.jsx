1a47ebc280edb6bc0ee95f92612da61e3793e2d2
Magnuscmj <56219481+Magnuscmj@users.noreply.github.com>
2021-03-11 19:36:37 +0100

    User can enter books, to books site

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -2,44 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Link } from 'react-router-dom';
 import { Route, Switch } from 'react-router';
-import { BookListPage } from "./BookListPage";
+import { BookListPage } from './BookListPage';
+import { CreateBookPage } from './CreateBookPage';
 
 function Application() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path={'/books'}>
-          <h1>List all the books</h1>
-        </Route>
-
-        <Route path={'/create'}>
-          <h1>Create a new book</h1>
-        </Route>
-
-        <Route path={'/create'}>
-          <h1>Create a new book</h1>
-        </Route>
-
-        <Route path={'/edit'}>
-          <h1>Edit an existing book</h1>
-        </Route>
-
-        <Route path={'/'}>
-          <h1>Book application home page</h1>
-          <ul>
-            <li>
-              <Link to={'/books'}>List books</Link>
-            </li>
-            <li>
-              <Link to={'/create'}>List books</Link>
-            </li>
-          </ul>
-        </Route>
-
-        <Route>
-          <h1>Page not found</h1>
-        </Route>
-      </Switch>
+      <nav>
+        <Link to={'/'}>Home</Link>
+      </nav>
+      <main>
+        <Switch>
+          <Route path={'/books'}>
+            <BookListPage />
+          </Route>
+          <Route path={'/create'}>
+            <CreateBookPage />
+          </Route>
+          <Route path={'/edit'}>
+            <h1>Edit an existing book</h1>
+          </Route>
+          <Route exact path={'/'}>
+            <h1>Book application home page</h1>
+            <ul>
+              <li>
+                <Link to={'/books'}>List books</Link>
+              </li>
+              <li>
+                <Link to={'/create'}>Add a book</Link>
+              </li>
+            </ul>
+          </Route>
+          <Route>Page not found</Route>
+        </Switch>
+      </main>
     </BrowserRouter>
   );
 }
--- src/server/server.js
+++ src/server/server.js
@@ -1,30 +1,44 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const books = [{
-  id: 1,
-  title: "Uncle Tom's Cabin",
-  author: "Harriet Stowe",
-  year: "1852"
-},
+const books = [
   {
-      id: 2,
-      title: "Hakkebakkeskogen",
-      author: "Thorbjørn Egner",
-      year: "1953"
-  }
-
+    id: 1,
+    title: "Uncle Tom's Cabin",
+    author: 'Harriet Stowe',
+    year: '1852',
+  },
+  {
+    id: 2,
+    title: 'Hakkebakkeskogen',
+    author: 'Thorbjørn Egner',
+    year: '1953',
+  },
 ];
 
-app.get("/api/books", (req, res) => {
-  res.json(books);
-})
-
+app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, '..', '..', 'dist')));
 
+app.get('/api/books', (req, res) => {
+  console.log(books);
+  res.json(books);
+});
+
+app.post('/api/books', (req, res) => {
+  const { title, author, year } = req.body;
+  console.log(req.body);
+  books.push({ title, author, year, id: books.length + 1 });
+  res.status(201);
+  res.end();
+});
+
 app.use((req, res, next) => {
+  if (req.method !== 'GET') {
+    return next();
+  }
   res.sendFile(path.resolve(__dirname, '..', '..', 'dist', 'index.html'));
 });
 
