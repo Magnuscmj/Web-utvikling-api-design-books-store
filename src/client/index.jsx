176623be925638454b2dc1c115e2deb83740eeb9
Magnuscmj <56219481+Magnuscmj@users.noreply.github.com>
2021-03-11 19:26:47 +0100

    Load books from api

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Link } from 'react-router-dom';
 import { Route, Switch } from 'react-router';
+import { BookListPage } from "./BookListPage";
 
 function Application() {
   return (
--- src/server/server.js
+++ src/server/server.js
@@ -3,10 +3,29 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname,"..", "..", "dist")));
+const books = [{
+  id: 1,
+  title: "Uncle Tom's Cabin",
+  author: "Harriet Stowe",
+  year: "1852"
+},
+  {
+      id: 2,
+      title: "Hakkebakkeskogen",
+      author: "Thorbjørn Egner",
+      year: "1953"
+  }
+
+];
+
+app.get("/api/books", (req, res) => {
+  res.json(books);
+})
+
+app.use(express.static(path.resolve(__dirname, '..', '..', 'dist')));
 
 app.use((req, res, next) => {
-  res.sendFile(path.resolve(__dirname,"..", "..", "dist", "index.html"));
+  res.sendFile(path.resolve(__dirname, '..', '..', 'dist', 'index.html'));
 });
 
 app.listen(3000, () => {
